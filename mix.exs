756d6366ca32b385dea07b18e80be4610015618e
Edgar Gomes <talktoedgar@gmail.com>
2023-01-16 19:36:36 +0000

    Adjust ABI

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule NervesSystemRpi3a64bit.MixProject do
         {"TARGET_ARCH", "aarch64"},
         {"TARGET_CPU", "cortex_a53"},
         {"TARGET_OS", "linux"},
-        {"TARGET_ABI", "gnueabihf"},
+        {"TARGET_ABI", "gnu"},
         {"TARGET_GCC_FLAGS",
          "-mabi=lp64 -fstack-protector-strong -mcpu=cortex-a53 -fPIE -pie -Wl,-z,now -Wl,-z,relro"}
       ],
